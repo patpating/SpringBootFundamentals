770f97031f997c2fa7d76ebb0455422efd58ebea
whynot <anil@panix.com>
2021-11-03 18:26:14 -0400

    Prespring 2

--- code/PreSpring/src/test/java/ttl/larku/service/StudentServiceTest.java
+++ code/PreSpring/src/test/java/ttl/larku/service/StudentServiceTest.java
@@ -1,13 +1,26 @@
 package ttl.larku.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ttl.larku.domain.Student;
-import ttl.larku.domain.Student.Status;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ttl.larku.domain.Student;
+import ttl.larku.domain.Student.Status;
+import ttl.larku.jconfig.LarkUConfig;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {LarkUConfig.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StudentServiceTest {
 
     private String name1 = "Bloke";
@@ -16,11 +29,20 @@ public class StudentServiceTest {
     private String phoneNumber1 = "290 298 4790";
     private String phoneNumber2 = "3838 939 93939";
 
+    @Autowired
     private StudentService studentService;
+
+    @Autowired
+    private ApplicationContext context;
 
     @BeforeEach
     public void setup() {
-        studentService = new StudentService();
+//        for(String name : context.getBeanDefinitionNames()) {
+//            System.out.println(name);
+//        }
+//        System.out.println(context.getBeanDefinitionCount() + " beans");
+
+//        studentService = new StudentService();
     }
 
     @Test
